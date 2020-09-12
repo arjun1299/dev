85122cc2903a8c4463e7df3513e9323ca5dde0b0
arjun1299 <arjun1299@gmail.com>
2020-09-12 19:18:19 +0530

    Removed duplication of phonemes from lang

--- code/listLang.py
+++ code/listLang.py
@@ -3,14 +3,17 @@ import csv
 path='phoible.csv'
 
 d={}
+## Lists all the languages along with their phonemes after extracting from phoible.csv
 
+## start with empty dictionary 
 with open(path, newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
          if (row['LanguageName']) not in d:
              d[row['LanguageName']]=[row['Phoneme'],]
          else:
-            d[row['LanguageName']].append(row['Phoneme'])
+            if(row['Phoneme'] not in d[row['LanguageName']]):
+                d[row['LanguageName']].append(row['Phoneme'])
     print(d)
 
 with open('listLang.csv', 'w', newline='') as csvfile:
